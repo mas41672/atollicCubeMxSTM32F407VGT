6d2b46128e36a3940ea033d6f1260744e09cf948
dpossardt <dirk.possardt1@freenet.de>
2018-08-06 13:39:54 +0200

    feat: Blink LED of GPIOD and Pin 12
    The source code adds a toggle function and a delay function to blink
    the green LED.

--- LED_Blink/Src/main.c
+++ LED_Blink/Src/main.c
@@ -101,7 +101,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-
+    HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+    HAL_Delay(1000);
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
